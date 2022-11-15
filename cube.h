5b29fd59c8d891d18e7f42ea2073dd14e3a4dacc
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-11-15 13:15:25 +0900

    first try done

--- cube.h
+++ cube.h
@@ -6,6 +6,28 @@
 #define TEXTURE_WIDTH        64
 #define TEXTURE_HEIGHT       64
 
+typedef struct s_game_info
+{
+	
+	t_parse_info parse;
+	t_player_info player;
+} t_game_info;
+
+typedef struct s_img
+{
+	unsigned int	*addr;
+	int				bpp;
+	int				size_line;
+	int				endian;
+}	t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+}	t_mlx;
+
 typedef struct s_color
 {
 	unsigned int	blue:8;
@@ -16,12 +38,12 @@ typedef struct s_color
 
 typedef struct s_parse_info
 {
-    char    	*path_north_texture;
-    char    	*path_south_texture;
-    char    	*path_west_texture;
-    char    	*path_east_texture;
+    int				*north_texture;
+    int				*south_texture;
+    int				*west_texture;
+    int				*east_texture;
     t_color 	floor;
-    t_color 	 ceil;
+    t_color 	ceiling;
 		int     	**map;
 } t_parse_info;
 
@@ -38,18 +60,20 @@ typedef enum e_hit
 	WALL_VERTICAL
 } t_hit;
 
+typedef enum e_texture_kind
+{
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST
+} t_texture_kind;
+
 typedef struct s_player_info
 {
 	t_vector	pos;
 	t_vector	dir;
 	t_vector	view;
 } t_player_info;
-
-typedef struct s_game_info
-{
-	t_parse_info parse;
-	t_player_info player;
-} t_game_info;
 
 typedef struct s_wall_pos
 {
@@ -63,5 +87,15 @@ typedef struct s_wall_info
 	double corrected_distance;
 	t_hit hit_side;
 } t_wall_info;
+
+typedef struct s_slice_info//single slice to work at
+{
+	int draw_top;//start-pixel in screen
+	int draw_bottom;
+	double texture_offset_x;
+	double texture_offset_y;//usually 0
+	double texture_step_y;//movement per screen pixel y (bigger step -> smaller at screen)
+	t_texture_kind texture_kind;
+} t_slice_info;
 
 #endif
