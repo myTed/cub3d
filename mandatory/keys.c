36e4ae46252fdd478770578713e78964f5773172
myTed <tribune11@nate.com>
2022-11-20 16:55:17 +0900

    회전 중복문제 해결

--- mandatory/keys.c
+++ mandatory/keys.c
@@ -1,12 +1,13 @@
 #include "cub3d.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 # define KEY_W			13
 # define KEY_A			0
 # define KEY_S			1
 # define KEY_D			2
 # define KEY_LEFT		123
-# define KEY_RIGHT	124
+# define KEY_RIGHT		124
 # define KEY_ESC		53
 
 int	exit_game(t_game_info *p_game, int status)
@@ -36,10 +37,8 @@ int	key_press(int keycode, t_game_info *p_game)
 		p_game->key.turn_left = PRESS;
 	else if (keycode == KEY_RIGHT)
 		p_game->key.turn_right = PRESS;
-
 	else if (keycode == KEY_ESC)
 		return (exit_game(p_game, EXIT_SUCCESS));
-
 	return (0);
 }
 
--- mandatory/update_player.c
+++ mandatory/update_player.c
@@ -61,34 +61,39 @@ int	move_player(t_game_info *p_game)
 
 int	turn_player(t_game_info *p_game)
 {
-	int	is_update;
+	int			is_update;
+	t_vector	old_dir;
+	t_vector	old_view;
 
 	is_update = TRUE;
+	ft_memcpy(&old_dir, &(p_game->player.dir), sizeof(t_vector));
+	ft_memcpy(&old_view, &(p_game->player.view), sizeof(t_vector));
 	if (p_game->key.turn_right == PRESS)
 	{
-			p_game->player.dir.x = \
-				p_game->player.dir.x * cos(TURN_SPEED) - p_game->player.dir.y * sin(TURN_SPEED);
-			p_game->player.dir.y = \
-				p_game->player.dir.x * sin(TURN_SPEED) + p_game->player.dir.y * cos(TURN_SPEED);
-			p_game->player.view.x = \
-				p_game->player.view.x * cos(TURN_SPEED) - p_game->player.view.y * sin(TURN_SPEED);
-			p_game->player.view.y = \
-				p_game->player.view.x * sin(TURN_SPEED) + p_game->player.view.y * cos(TURN_SPEED);
+		p_game->player.dir.x = \
+			old_dir.x * cos(TURN_SPEED) - old_dir.y * sin(TURN_SPEED);
+		p_game->player.dir.y = \
+			old_dir.x * sin(TURN_SPEED) + old_dir.y * cos(TURN_SPEED);
+		p_game->player.view.x = \
+			old_view.x * cos(TURN_SPEED) - old_view.y * sin(TURN_SPEED);
+		p_game->player.view.y = \
+			old_view.x * sin(TURN_SPEED) + old_view.y * cos(TURN_SPEED);
 		p_game->key.turn_right = RELEASE;
+	}
+	else if (p_game->key.turn_left == PRESS)
+	{
+		p_game->player.dir.x = \
+			old_dir.x * cos(-TURN_SPEED) - old_dir.y * sin(-TURN_SPEED);
+		p_game->player.dir.y = \
+			old_dir.x * sin(-TURN_SPEED) + old_dir.y * cos(-TURN_SPEED);
+		p_game->player.view.x = \
+			old_view.x * cos(-TURN_SPEED) - old_view.y * sin(-TURN_SPEED);
+		p_game->player.view.y = \
+			old_view.x * sin(-TURN_SPEED) + old_view.y * cos(-TURN_SPEED);
+		p_game->key.turn_left = RELEASE;
 	}
 	else
 		is_update = FALSE;
-	//if (p_game->key.turn_right == PRESS)
-	//{
-	//		p_game->player.dir.x = \
-	//			p_game->player.dir.x * cos(TURN_SPEED) - p_game->player.dir.y * sin(TURN_SPEED);
-	//		p_game->player.dir.y = \
-	//			p_game->player.dir.x * sin(TURN_SPEED) + p_game->player.dir.y * cos(TURN_SPEED);
-	//		p_game->player.view.x = \
-	//			p_game->player.view.x * cos(TURN_SPEED) - p_game->player.view.y * sin(TURN_SPEED);
-	//		p_game->player.view.y = \
-	//			p_game->player.view.x * sin(TURN_SPEED) + p_game->player.view.y * cos(TURN_SPEED);
-	//}
 	return (is_update);
 }
 
@@ -98,6 +103,6 @@ int	update_player(t_game_info *p_game)
 
 	is_update = FALSE;
 	is_update = move_player(p_game);
-	//is_update = turn_player(p_game);
+	is_update |= turn_player(p_game);
 	return (is_update);
 }
