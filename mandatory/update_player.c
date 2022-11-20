4a74202055055cb6ce3b972028ceafacb033216f
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-11-20 12:26:11 +0900

    임시

--- mandatory/update_player.c
+++ mandatory/update_player.c
@@ -9,20 +9,38 @@
 #define TRUE 1
 #define FALSE 0
 
-//int	is_wall()
+#define WALL 1
+
+int	is_wall(t_game_info *p_game, double delta_x, double delta_y)
+{
+	int	new_x;
+	int	new_y;
+
+	printf("%s\n", (char *)p_game->parse.map[0]);
+
+	new_x = (int)(p_game->player.pos.x + delta_x);
+	new_y = (int)(p_game->player.pos.y + delta_y);
+	if (p_game->parse.map/*.data*/[new_y][new_x] == WALL)
+		return (TRUE);
+	else
+		return (FALSE);
+}
 
 int	move_player(t_game_info *p_game)
 {
 	int	is_update;
 
 	is_update = TRUE;
-	if (p_game->key.move_forward == PRESS/* && is_wall() == FALSE)*/)
-		p_game->player.pos.y += (p_game->player.dir.y * MOVE_SPEED);
-	else if (p_game->key.move_backward == PRESS/* && is_wall() == FALSE)*/)
+	if (p_game->key.move_forward == PRESS && \
+		is_wall(p_game, 0, p_game->player.dir.y * MOVE_SPEED) == FALSE)
+	{
+			p_game->player.pos.y += (p_game->player.dir.y * MOVE_SPEED);
+	}
+	else if (p_game->key.move_backward == PRESS/* && is_wall() == FALSE*/)
 		p_game->player.pos.y -= (p_game->player.dir.y * MOVE_SPEED);
-	else if (p_game->key.move_left == PRESS/* && is_wall() == FALSE)*/)
+	else if (p_game->key.move_left == PRESS/* && is_wall() == FALSE*/)
 		p_game->player.pos.x -= (p_game->player.view.x * MOVE_SPEED);
-	else if (p_game->key.move_right == PRESS/* && is_wall() == FALSE)*/)
+	else if (p_game->key.move_right == PRESS/* && is_wall() == FALSE*/)
 		p_game->player.pos.x += (p_game->player.view.x * MOVE_SPEED);
 	else
 		is_update = FALSE;
