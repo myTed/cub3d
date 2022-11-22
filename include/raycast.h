15c17cb8020146a8f428b643caa0114d818234b7
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-11-22 14:47:56 +0900

    헤더도 푸시

---
+++ include/raycast.h
@@ -0,0 +1,52 @@
+#ifndef RAYCAST_H
+#define RAYCAST_H
+
+/*
+ * UTILS
+ */
+
+typedef enum e_hit
+{
+	NO = 0,
+	HORIZON,
+	VERTICAL
+}	t_hit;
+
+typedef struct s_wall_pos
+{
+	int	x;
+	int	y;
+}	t_wall_pos;
+
+/*
+ * KEY STRUCTS 
+ */
+
+typedef struct s_wall_dist_info
+{
+	double	vertical;
+	double	horizon;
+	double	delta_vertical;
+	double	delta_horizon;
+	int			step_vertical;
+	int			step_horizon;
+} t_wall_dist_info;
+
+typedef struct s_wall_info
+{
+	double 			corrected_distance;
+	t_wall_pos	pos;
+	t_hit				hit_side;
+}	t_wall_info;
+
+typedef struct s_slice_info
+{
+	t_img			*p_texture_img;
+	int				draw_top;
+	int				draw_bottom;
+	double		texture_offset_x;
+	double		texture_offset_y;
+	double		texture_step_y;
+} t_slice_info;
+
+#endif
